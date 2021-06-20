48b4cefd82cba5ef84d2ff8c2161faef6eebac96
wilsonelwilson <wvmc_es@hotmail.com>
2021-06-20 12:58:38 -0500

    Add testimonial

--- public/js/script.js
+++ public/js/script.js
@@ -4849,8 +4849,33 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
   NavigationOptions: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true
-  }
+  grabCursor: true,
+  // watchSlidesProgress:true,
+  // slidesOffsetAfter:50,
+  // slidesOffsetBefore:50,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    // when window width is >= 480px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 640px
+    960: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  } // pagination: {
+  // el: ".swiper-pagination",
+  // clickable: true,
+  // },
+
 });
